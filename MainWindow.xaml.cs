b45536a52fb16957c7c2a06add0760afef87099b
Oli <oliverposa@outlook.com>
2023-06-13 08:36:45 +0800

    Upgrade to .net 7. Added System.Management.Automation to references.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Management.Automation;
-
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using SystemModerator.Properties;
 
 namespace SystemModerator
 {
